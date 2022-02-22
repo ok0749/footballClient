992e58959347603c3c9aedf4e8e129c96cd0fbbe
donghak <donghak@donghaks-MacBook-Air.local>
2022-02-22 22:38:58 +0900

    stadiums/save 라우트, static 경로 설정

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(port, () => {
-  console.log(`footballClient app listening on port ${port}`);
+app.get("/stadiums/save", (req, res) => {
+  res.render("stadiums-save");
 });
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port, () =>
+    console.log(`footballClient app listening on port ${port}`)
+  );
+}
+
+export default app;
