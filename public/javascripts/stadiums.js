cfb67e3967ac1d5afbcc0fb730d081b1b8f370af
donghak <donghak@donghaks-MacBook-Air.local>
2022-02-23 13:54:54 +0900

    ajax 공통 함수로 만듬

--- public/javascripts/stadiums.js
+++ public/javascripts/stadiums.js
@@ -1,26 +1,43 @@
+const ajax = (type, url, data) => {
+  $.ajax({
+    type,
+    url,
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify(data),
+  });
+};
+
 const main = {
   init: function () {
     $("#btn-save").on("click", () => this.save());
   },
 
-  save: function () {
+  save: async function () {
     const data = {
       name: $("#name").val(),
       location: $("#location").val(),
     };
 
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8080/api/v1/stadiums",
-      dataType: "json",
-      contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(data),
-    })
-      .done(() => {
-        alert("경기장이 등록되었습니다");
-        window.location.href = "/";
-      })
-      .fail((error) => alert(JSON.stringify(error)));
+    try {
+      await ajax("POST", "http://localhost:8080/api/v1/stadiums", data);
+      alert("경기장이 등록되었습니다.");
+      window.location.href = "/";
+    } catch (error) {
+      alert(JSON.stringify(error));
+    }
+    // $.ajax({
+    //   type: "POST",
+    //   url: "http://localhost:8080/api/v1/stadiums",
+    //   dataType: "json",
+    //   contentType: "application/json; charset=utf-8",
+    //   data: JSON.stringify(data),
+    // })
+    //   .done(() => {
+    //     alert("경기장이 등록되었습니다");
+    //     window.location.href = "/";
+    //   })
+    //   .fail((error) => alert(JSON.stringify(error)));
   },
 };
 
